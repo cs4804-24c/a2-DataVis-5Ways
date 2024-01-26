70859360a2c52b7a3bcc29ed3471c1ca7f2b63f1
cehrensperger <cehrensperger49@gmail.com>
2024-01-25 20:59:12 -0500

    Added some design/technical achievements.

--- Altair/altairVis.py
+++ Altair/altairVis.py
@@ -2,18 +2,59 @@ import altair as alt
 # from vega_datasets import data
 url = '../penglings.csv'
 
-chart = alt.Chart(url).mark_circle().encode(
+options = ['Adelie', 'Chinstrap', 'Gentoo']
+labels = [option + ' ' for option in options]
+
+input_dropdown = alt.binding_radio(
+    options=options + [None],
+    labels=labels + ['All'],
+    name='Species of penguin: ' 
+)
+
+selection = alt.selection_point(
+    fields=['species'],
+    bind=input_dropdown,
+)
+
+time_interval_selector = alt.selection_interval(encodings=['x'], name='year')
+
+chart = alt.Chart(url).transform_calculate(
+
+    url='https://www.google.com/search?q=' + alt.datum.species + '+penguin',
+
+).mark_circle().encode(
     # set x-axis scale limits
     x=alt.X('flipper_length_mm:Q', scale=alt.Scale(domain=(170, 235))),
     y=alt.Y('body_mass_g:Q', scale=alt.Scale(domain=(2500, 6500))),
     # radius porportional to bill_length_mm
     size=alt.Size('bill_length_mm:Q', scale=alt.Scale(range=[10, 200], domain=[30, 55])),
     # color corresponds to species
-    color='species:N',
-    # set point to be filled circle
-    fill='species:N'
+    color=alt.Color('species:N').scale(domain=options, range=['#ff8d05', '#9934cc', '#048b8c']),
+    tooltip=['species:N', 'bill_length_mm:Q', 'body_mass_g:Q', 'flipper_length_mm:Q'],
+    href='url:N',
+).interactive().add_params(
+    selection,
+    # slider_selection
+).transform_filter(
+    selection
+).transform_filter(
+    time_interval_selector
+).properties(
+    width=600,
+    height=400
 )
 
+timeline = alt.Chart(url).mark_line().encode(
+    x='year:O',
+    y='count()',
+).properties(
+    height=50,
+    width=400
+).add_params(
+    time_interval_selector
+)
+
+chart = alt.vconcat(chart, timeline)
 
 chart.save('chart.html')
 

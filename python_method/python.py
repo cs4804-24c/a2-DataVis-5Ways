694c0518a11226df72172e590809b63020f99336
JinjiaOu <129883716+JinjiaOu@users.noreply.github.com>
2024-01-29 23:12:06 -0500

    Python(Pandas + Matplotlib) method

---
+++ python_method/python.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file_path = r'E:\IdeaProjects\python_method\penglings.csv'
+data = pd.read_csv(file_path)
+
+data_clean = data.dropna(subset=['flipper_length_mm', 'body_mass_g', 'bill_length_mm'])
+
+color_mapping = {
+    'Adelie': 'orange',
+    'Chinstrap': 'purple',
+    'Gentoo': 'teal'
+}
+
+plt.figure(figsize=(10, 6))
+
+for species, group in data_clean.groupby('species'):
+    plt.scatter(
+        group['flipper_length_mm'],
+        group['body_mass_g'],
+        s=group['bill_length_mm'] * 5,
+        alpha=0.8,
+        label=species,
+        color=color_mapping.get(species)
+    )
+
+plt.xlim(150, data_clean['flipper_length_mm'].max())
+plt.ylim(2000, data_clean['body_mass_g'].max())
+
+plt.xlabel('Flipper Length (mm)')
+plt.ylabel('Body Mass (g)')
+plt.title('Scatter Plot of Penguins')
+
+plt.legend(title='Species')
+
+plt.show()
+
+plt.savefig(r'E:\IdeaProjects\python_method\penguin_scatterplot.png')
